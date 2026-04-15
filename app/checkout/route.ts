1f1a5c359cb0b373e9c1a2bd84a93c9345e6a412
Alen Pepa <xalenpepa@gmail.com>
2026-04-15 07:22:15 -0700

    Update route.ts

--- app/checkout/route.ts
+++ app/checkout/route.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 import { z } from 'zod';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-01-27.acacia',
+  apiVersion: '2026-03-25.dahlia',
 });
 
 const checkoutSchema = z.object({
